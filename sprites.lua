f02a34a125acfd7ee7c5281425d3ead74503e854
Addison Mink <addison.mink@example.com>
2026-01-22 15:54:29 -0600

    add sprites.

--- sprites.lua
+++ sprites.lua
@@ -13,5 +13,11 @@ npc = {
   priestess = { sx = 72, sy = 0 },
   candle = { sx = 88, sy = 0 },
   fairies = { sx = 104, sy = 0 },
-  sage = { sx = 0, sy = 32, size = 24 }
+  sage = { sx = 0, sy = 32, size = 24 },
+  goblin = { sx = 48, sy = 64 },
+  goblin_mage = { sx = 64, sy = 64 },
+  goblin_warrior = { sx = 104, sy = 32 },
+  zombie = { sx = 80, sy = 64 },
+  ghost = { sx = 96, sy = 64 },
+  dragon = { sx = 112, sy = 64 }
 }

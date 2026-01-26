c28d3628df907c09be451e96d1faa81f05dcb6b0
Addison Mink <addison.mink@example.com>
2026-01-25 19:26:10 -0600

    add money to hud.

--- global.lua
+++ global.lua
@@ -75,9 +75,11 @@ function draw_hud()
   local hp_str = "hp " .. global.player.hp .. "/" .. global.player.max_hp
   local mp_str = "mp " .. global.mp .. "/" .. global.max_mp
   local item_str = "item " .. global.item_count .. "/" .. global.max_item_count
+  local money_str = "$" .. global.money
 
   rectfill(0, 0, 127, 11, 0)
   print(hp_str, 4, 4, 8)
-  print(mp_str, 64 - #mp_str * 2, 4, 12)
+  print(mp_str, 42, 4, 12)
+  print(money_str, 74, 4, 10)
   print(item_str, 124 - #item_str * 4, 4, 15)
 end
